212f6b8e5760c02e48ca5f03d254e3473eff0ed8
Marmik Patel <marmikpatel250@gmail.com>
2022-04-06 11:40:15 +0530

    created production ready app

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './router/AppRouter';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+if(process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +26,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
